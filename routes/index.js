c80141d0390f42ffd1c8ecbd37e5faeced667a0f
Alejandro Sanabria <alesanabriav@gmail.com>
2015-09-17 10:04:11 -0500

    added api

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Guest = require('../models/guest');
 
+router.get('/api/guests', function(req, res) {
+  Guest.find({}).exec(function(err, guests) {
+    return res.status(200).json(guests);
+  });
+});
+
 router.post('/api/guests', function(req, res) {
   var guest = new Guest(req.body);
   guest.save(function(err, guestStored){
