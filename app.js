6d669f8ed4e3b28ed14d65c12bcbbc820d145964
Alejandro Sanabria <alesanabriav@gmail.com>
2015-09-17 09:59:07 -0500

    added comment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/1nov');
+mongoose.connect('mongodb://127.0.0.1/1nov');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- models/guest.js
+++ models/guest.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var guestSchema = mongoose.Schema({
   names: String,
   email: String,
-  phone: String
+  phone: String,
+  comment: String
 });
 
 module.exports = mongoose.model('Guest', guestSchema);
